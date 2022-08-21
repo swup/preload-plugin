bd882711d23d1ae60e82b2686707c4e02655d27e
Philipp Daun <post@philippdaun.net>
2022-08-21 12:49:20 +0200

    Build dist files

--- dist/SwupPreloadPlugin.js
+++ dist/SwupPreloadPlugin.js
@@ -286,13 +286,13 @@ var PreloadPlugin = function (_Plugin) {
                             var page = swup.getPageData(response);
                             if (page != null) {
                                 page.url = link.getAddress();
-                                swup.cache.cacheUrl(page, swup.options.debugMode);
+                                swup.cache.cacheUrl(page);
                                 swup.triggerEvent('pagePreloaded');
+                                resolve(page);
                             } else {
                                 reject(link.getAddress());
                                 return;
                             }
-                            resolve(swup.cache.getPage(link.getAddress()));
                         }
                     });
                 } else {
