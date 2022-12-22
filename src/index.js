05d71b5d5e78e1798821176036caec52b8e9b3a8
Rasso Hilber <mail@rassohilber.com>
2022-12-22 09:57:40 +0100

    Add and apply `npm run lint`

--- src/index.js
+++ src/index.js
@@ -4,128 +4,130 @@ import { queryAll } from 'swup/lib/utils';
 import { Link, getCurrentUrl, fetch } from 'swup/lib/helpers';
 
 export default class PreloadPlugin extends Plugin {
-    name = "PreloadPlugin";
+	name = 'PreloadPlugin';
 
-    mount() {
-        const swup = this.swup;
+	mount() {
+		const swup = this.swup;
 
-        if (!swup.options.cache) {
-            console.warn('PreloadPlugin: swup cache needs to be enabled for preloading');
-            return;
-        }
+		if (!swup.options.cache) {
+			console.warn('PreloadPlugin: swup cache needs to be enabled for preloading');
+			return;
+		}
 
-        swup._handlers.pagePreloaded = [];
-        swup._handlers.hoverLink = [];
+		swup._handlers.pagePreloaded = [];
+		swup._handlers.hoverLink = [];
 
-        swup.preloadPage = this.preloadPage;
-        swup.preloadPages = this.preloadPages;
+		swup.preloadPage = this.preloadPage;
+		swup.preloadPages = this.preloadPages;
 
-        // register mouseover handler
-        swup.delegatedListeners.mouseover = delegate(
-            document.body,
-            swup.options.linkSelector,
-            'mouseover',
-            this.onMouseover.bind(this)
-        );
+		// register mouseover handler
+		swup.delegatedListeners.mouseover = delegate(
+			document.body,
+			swup.options.linkSelector,
+			'mouseover',
+			this.onMouseover.bind(this)
+		);
 
-        // initial preload of links with [data-swup-preload] attr
-        swup.preloadPages();
+		// initial preload of links with [data-swup-preload] attr
+		swup.preloadPages();
 
-        // do the same on every content replace
-        swup.on('contentReplaced', this.onContentReplaced)
+		// do the same on every content replace
+		swup.on('contentReplaced', this.onContentReplaced);
 
-        // cache unmodified dom of initial/current page
-        swup.preloadPage(getCurrentUrl());
-    }
+		// cache unmodified dom of initial/current page
+		swup.preloadPage(getCurrentUrl());
+	}
 
-    unmount() {
-        const swup = this.swup;
+	unmount() {
+		const swup = this.swup;
 
-        if (!swup.options.cache) {
-            return;
-        }
+		if (!swup.options.cache) {
+			return;
+		}
 
-        swup._handlers.pagePreloaded = null;
-        swup._handlers.hoverLink = null;
+		swup._handlers.pagePreloaded = null;
+		swup._handlers.hoverLink = null;
 
-        swup.preloadPage = null;
-        swup.preloadPages = null;
+		swup.preloadPage = null;
+		swup.preloadPages = null;
 
-        swup.delegatedListeners.mouseover.destroy();
+		swup.delegatedListeners.mouseover.destroy();
 
-        swup.off('contentReplaced', this.onContentReplaced)
-    }
+		swup.off('contentReplaced', this.onContentReplaced);
+	}
 
-    onContentReplaced = () => {
-        this.swup.preloadPages();
-    }
+	onContentReplaced = () => {
+		this.swup.preloadPages();
+	};
 
-    /**
-     * Apply swup.ignoreLink (will become available in swup@3)
-     */
-    ignoreLink(linkEl) {
-        if (typeof this.swup.ignoreLink === 'function') {
-            return this.swup.ignoreLink(linkEl);
-        }
-        return false;
-    }
+	/**
+	 * Apply swup.ignoreLink (will become available in swup@3)
+	 */
+	ignoreLink(linkEl) {
+		if (typeof this.swup.ignoreLink === 'function') {
+			return this.swup.ignoreLink(linkEl);
+		}
+		return false;
+	}
 
-    onMouseover = event => {
-        const swup = this.swup;
-        const linkEl = event.delegateTarget;
+	onMouseover = (event) => {
+		const swup = this.swup;
+		const linkEl = event.delegateTarget;
 
-        if (this.ignoreLink(linkEl)) return;
+		if (this.ignoreLink(linkEl)) return;
 
-        swup.triggerEvent('hoverLink', event);
+		swup.triggerEvent('hoverLink', event);
 
-        const link = new Link(linkEl);
-        if (
-            link.getAddress() !== getCurrentUrl() &&
-            !swup.cache.exists(link.getAddress()) &&
-            swup.preloadPromise == null
-        ) {
-            swup.preloadPromise = swup.preloadPage(link.getAddress());
-            swup.preloadPromise.route = link.getAddress();
-            swup.preloadPromise
-                .finally(() => {
-                    swup.preloadPromise = null;
-                });
-        }
-    }
+		const link = new Link(linkEl);
+		if (
+			link.getAddress() !== getCurrentUrl() &&
+			!swup.cache.exists(link.getAddress()) &&
+			swup.preloadPromise == null
+		) {
+			swup.preloadPromise = swup.preloadPage(link.getAddress());
+			swup.preloadPromise.route = link.getAddress();
+			swup.preloadPromise.finally(() => {
+				swup.preloadPromise = null;
+			});
+		}
+	};
 
-    preloadPage = pathname => {
-        const swup = this.swup;
+	preloadPage = (pathname) => {
+		const swup = this.swup;
 
-        let link = new Link(pathname);
-        return new Promise((resolve, reject) => {
-            if (!swup.cache.exists(link.getAddress())) {
-                fetch({ url: link.getAddress(), headers: swup.options.requestHeaders }, (response) => {
-                    if (response.status === 500) {
-                        swup.triggerEvent('serverError');
-                        reject(link.getAddress());
-                    } else {
-                        // get json data
-                        let page = swup.getPageData(response);
-                        if (page != null) {
-                            page.url = link.getAddress();
-                            swup.cache.cacheUrl(page);
-                            swup.triggerEvent('pagePreloaded');
-                            resolve(page);
-                        } else {
-                            reject(link.getAddress());
-                            return;
-                        }
-                    }
-                });
-            } else {
-                resolve(swup.cache.getPage(link.getAddress()));
-            }
-        });
-    };
+		let link = new Link(pathname);
+		return new Promise((resolve, reject) => {
+			if (!swup.cache.exists(link.getAddress())) {
+				fetch(
+					{ url: link.getAddress(), headers: swup.options.requestHeaders },
+					(response) => {
+						if (response.status === 500) {
+							swup.triggerEvent('serverError');
+							reject(link.getAddress());
+						} else {
+							// get json data
+							let page = swup.getPageData(response);
+							if (page != null) {
+								page.url = link.getAddress();
+								swup.cache.cacheUrl(page);
+								swup.triggerEvent('pagePreloaded');
+								resolve(page);
+							} else {
+								reject(link.getAddress());
+								return;
+							}
+						}
+					}
+				);
+			} else {
+				resolve(swup.cache.getPage(link.getAddress()));
+			}
+		});
+	};
 
-    preloadPages = () => {
-        queryAll('[data-swup-preload]').forEach((element) => {
-            this.swup.preloadPage(element.href);
-        });
-    };
+	preloadPages = () => {
+		queryAll('[data-swup-preload]').forEach((element) => {
+			this.swup.preloadPage(element.href);
+		});
+	};
 }
