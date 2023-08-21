dc2ac4328e3c5ca07edd40dd7752b0236cb287aa
Philipp Daun <post@philippdaun.net>
2023-08-21 12:26:10 +0200

    Add comments

--- src/queue.ts
+++ src/queue.ts
@@ -2,20 +2,29 @@ type QueueFunction = {
 	(): unknown | Promise<unknown>;
 };
 
+/**
+ * A priority queue that runs a limited number of jobs at a time.
+ */
 export default class Queue {
+	/** The number of jobs to run at a time */
 	private limit: number;
+	/** The queue of low-priority jobs */
 	private qlow: Map<string, QueueFunction> = new Map();
+	/** The queue of high-priority jobs */
 	private qhigh: Map<string, QueueFunction> = new Map();
+	/** The list of currently running jobs */
 	private running: Set<string> = new Set();
 
 	constructor(limit: number = 1) {
 		this.limit = limit;
 	}
 
+	/** The total number of jobs in the queue */
 	get total(): number {
 		return this.qlow.size + this.qhigh.size;
 	}
 
+	/** Add a job to queue */
 	add(key: string, fn: QueueFunction, highPriority: boolean = false): void {
 		if (this.running.has(key)) return;
 
@@ -34,6 +43,7 @@ export default class Queue {
 		}
 	}
 
+	/** Run the next available job */
 	protected async run(): Promise<void> {
 		if (!this.total) return;
 		if (this.running.size >= this.limit) return;
@@ -48,6 +58,7 @@ export default class Queue {
 		}
 	}
 
+	/** Get the next available job */
 	protected next(): { key: string; fn: QueueFunction } | null {
 		return [this.qhigh, this.qlow].reduce((acc, queue) => {
 			if (!acc) {
