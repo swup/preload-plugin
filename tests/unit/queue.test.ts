78a668258b6d23aaa2410e2d3d66fb05b3c01c46
Philipp Daun <post@philippdaun.net>
2023-12-15 11:38:16 +0100

    Add test for high-priority queue items

--- tests/unit/queue.test.ts
+++ tests/unit/queue.test.ts
@@ -56,7 +56,7 @@ describe('sequencing', () => {
 		expect(elapsed).to.be.lte(max);
 	});
 
-	it('allow custom queue size', async () => {
+	it('allows custom queue size', async () => {
 		let size = 5;
 		let items = 5;
 		let duration = 100;
@@ -76,3 +76,29 @@ describe('sequencing', () => {
 	});
 });
 
+describe('priorization', () => {
+	it('prioritizes high-priority items', async () => {
+		let size = 1;
+		let items = 3;
+		let duration = 100;
+		let elapsed = 0;
+		const seen: number[] = [];
+
+		const { add, next } = createQueue(size);
+		const timer = createTimer();
+		const testLow = () => sleep(duration).then(() => ((seen.push(0), (elapsed = timer())))).then(next);
+		const testHigh = () => sleep(duration).then(() => ((seen.push(1), (elapsed = timer())))).then(next);
+		for (let i = 0; i < items; i++) {
+			add(() => testLow());
+		}
+		for (let i = 0; i < items; i++) {
+			add(() => testHigh(), true);
+		}
+		await sleep((2 * items + 1) * duration);
+
+		const [min, max] = pad(2 * items * duration);
+		expect(elapsed).to.be.gte(min);
+		expect(elapsed).to.be.lte(max);
+		expect(seen).toEqual([0, 1, 1, 1, 0, 0]);
+	});
+});
