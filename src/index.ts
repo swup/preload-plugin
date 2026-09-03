6200899ebb7fbcd21b38d843081ec691e7cda6ae
Philipp Daun <post@philippdaun.net>
2026-09-03 16:58:30 +0200

    Inject page load hook args during preload

--- src/index.ts
+++ src/index.ts
@@ -181,10 +181,15 @@ export default class SwupPreloadPlugin extends Plugin {
 	/**
 	 * Before core page load: return existing preload promise if available.
 	 */
-	protected onPageLoad: HookDefaultHandler<'page:load'> = (visit, args, defaultHandler) => {
+	protected onPageLoad: HookDefaultHandler<'page:load'> = async (visit, args, defaultHandler) => {
 		const { url } = visit.to;
-		if (url && this.preloadPromises.has(url)) {
-			return this.preloadPromises.get(url) as Promise<PageData>;
+		const preloadPromise = url ? this.preloadPromises.get(url) : undefined;
+		// A preload can fail and end up with no page to reuse
+		const page = (await preloadPromise?.catch(() => undefined)) ?? null;
+		if (page) {
+			args.page = page;
+			args.cache = false;
+			return page;
 		}
 		return defaultHandler!(visit, args);
 	};
